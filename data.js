945203a109c74b1c31b8d861b9164a40bf0dc349
Kurt Van Etten <kurt@vanetten.us>
2019-04-27 12:59:25 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,15 @@
+module.exports = {
+  data: [
+      {
+        userid: "user",
+        id: 0,
+        title: "An Item",
+        category: "Misc",
+        description: "This is my first item",
+        link: "url will go here",
+        completed: false,
+        created: null,
+        updated: null
+    }
+  ]
+};
